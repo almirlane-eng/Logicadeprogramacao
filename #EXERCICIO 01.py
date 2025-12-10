641da4a95e2067112b857f96e8a564e564add52c
almirlane-eng <almirlane@gmail.com>
2025-12-10 20:59:10 -0300

    Add files via upload

---
+++ #EXERCICIO 01.py
@@ -0,0 +1,32 @@
+#EXERCICIO 01
+#crie um script,que peça para o usuario digitar um número inteiro, depois outro numero , e ao final exiba o resultado
+
+numero_1=int(input("digite um numero inteiro: "))
+
+numero_2=float(input("digite outro nuemro: "))
+
+print(numero_1+numero_2)
+
+#EXERCICIO 02
+rint("hello world")
+
+saudacao="hello world2"
+
+print(saudacao)
+
+print('1+1')
+
+print('1'+'1')
+
+nome=input("Qual o seu nome?")
+idade=input("Qual a sua idade?")
+peso=input("Qual o seu peso?")
+
+print(nome,idade,peso)
+
+print('Meu nome é',nome, 'eu tenho',idade,',eu tenho',peso,)
+
+print('Meu nome é {n}, eu tenho {i} anos, meu peso e {p}'.format(n=nome,i=idade,p=peso))
+
+print(f'Meu nome é {nome}, eu tenho {idade} anos, meu peso e {peso}')
+
