4bd93030fed88e133c502fd7ad4dabcd4ee16be5
almirlane-eng <almirlane@gmail.com>
2025-12-10 20:58:35 -0300

    Add files via upload

---
+++ #EXERCICIO 02.py
@@ -0,0 +1,22 @@
+#EXERCICIO 02
+imprimir=("olá,mundo")
+
+saudação="ola, mundo 2"
+
+imprimir=("1+1")
+
+imprimir=('1'+'1')
+
+nome=input("Qual o seu nome?")
+
+idade=input("Qual sua idade?")
+
+peso=input("Qual o seu peso?")
+
+imprimir=(nome,idade,peso)
+
+imprimir=("meu nome é",nome,"eu tenho",idade,"eu tenho", peso,)
+
+imprimir=("meu nome é {n},eu tenho{i} anos, meu peso e {p}".formatar(n=nome,eu=iade,p=peso))
+
+imprimir=(f"meu nome é{nome}, eu tenho{idade}anos,meu peso e {peso}")
