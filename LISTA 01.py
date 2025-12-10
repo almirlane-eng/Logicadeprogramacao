0cd4c4d31f04e0702691c418149efe4224cb9c66
almirlane-eng <almirlane@gmail.com>
2025-12-10 10:54:02 -0300

    Add files via upload

---
+++ LISTA 01.py
@@ -0,0 +1,57 @@
+#QUESTÃO 01#
+contador = 1
+while contador <= 20 :
+    print(contador)
+    contador += 1
+    
+
+#QUESTÃO 02
+numero = int(input("Digite um número: "))
+
+while numero >= 0:
+    print(numero)
+    numero -= 1
+
+print("fim da contagem regressiva")
+    
+
+#QUESTÃO# 03
+
+soma = 0
+contador = 1
+while contador <= 5:
+    numero = float(input(f"Digite o {contador}° numero:"))
+    soma += numero
+    contador +=
+
+print("A soma dos número é:",soma)    
+
+#QUESTÃO 04#
+nome_correto = "Lane"
+nome = ""
+
+while nome != nome_correto:
+    nome = str(input("Acerte o meu nome: "))
+
+print("Parabéns Acertou!")
+
+#Questão 05#
+
+soma_idade = 0
+contador = 0
+while True:
+
+    idade =int(input("Digite a proxima idade, ou digite 0 para sair: ")
+    if idade == 0
+       break
+    soma_idade+=soma_idade
+    contador+=
+
+print("quantidade de pessoas",contador)
+print("soma das idades",soma_idade)
+print("media das iades do grupo",soma_idade/contador)
+
+
+
+
+
