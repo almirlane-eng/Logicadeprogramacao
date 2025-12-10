c249521f30f743e17339351493e3f073409463eb
almirlane-eng <almirlane@gmail.com>
2025-12-10 19:26:33 -0300

    Add files via upload

---
+++ Desafios.py
@@ -0,0 +1,16 @@
+#DESAFIO 01
+frutas=["melão","uva","laranja","acerola"]
+
+imprima(frutas[0])
+
+frutas[0]="morango"
+
+print(frutas)
+
+#Desafio 02
+cores=["azul","lilas","branco","verde"]
+imprimir(nucleos)
+nucleos.acrescentar("preto")
+nucleos.acrescentar("papel picado")
+nucleos.organizar()
+imprimir(nucleos)
